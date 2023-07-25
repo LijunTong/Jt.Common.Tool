c03ec181ef917d9e6089fb83b17b5ba55a2a9f0e
joont <1193247828@qq.com>
2023-07-25 18:01:47 +0800

    fix:增加单元测试项目

---
+++ src/Jt.Common.ToolTests.API/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Jt.Common.ToolTests.API
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+    }
+}
--- src/Jt.Common.ToolTests/Extension/HttpClientExtensionTests.cs
+++ src/Jt.Common.ToolTests/Extension/HttpClientExtensionTests.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using Jt.Common.Tool.Extension;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
 
 namespace Jt.Common.Tool.Extension.Tests
 {
-    [TestClass()]
+    [TestFixture()]
     public class HttpClientExtensionTests
     {
-        [TestMethod()]
-        public void GetAsyncTest()
+        private HttpClient _httpClient;
+
+        public HttpClientExtensionTests()
         {
-            Assert.Fail();
+            _httpClient = new HttpClient();
+        }
+
+        [Test()]
+        public async Task DoGetAsyncTest()
+        {
+            var param = new Dictionary<string, object>
+            {
+                { "name", "apple" },
+                { "age", 12 }
+            };
+            var data = await _httpClient.DoGetAsync("http://localhost:5000/WeatherForecast/GetParam", param);
+            Debug.WriteLine(data);
+            Assert.IsNotEmpty(data);
+        }
+
+        [Test()]
+        public async Task DoPostAsyncTest()
+        {
+            var param = new Dictionary<string, object>
+            {
+                { "name", "apple" },
+                { "age", 12 }
+            };
+            var data = await _httpClient.DoPostAsync("http://localhost:5000/WeatherForecast/PostParam", param.ToJson());
+            Debug.WriteLine(data);
+            Assert.IsNotEmpty(data);
         }
     }
 }
